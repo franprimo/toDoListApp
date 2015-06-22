6a726edf7b6dec1c16a59923e73a798cddfe7b60
Fran Primo Ruiz <FranPrimo@MacBook-de-Fran.local>
2015-06-22 22:50:57 +0200

    to do list app ready!

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,16 +8,57 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+//Creo un array sin inicializar.
+var toDoList = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet weak var toDoTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil {
+            toDoList = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Indicamos cuantas celdas va a tener. Tantas como valores tenga el array que la pasamos.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+
+    //Crea el contenido de las celdas.
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = toDoList[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        //Si deslizo el dedo hacia la izquierda, elimino del array el valor que se encuentra en esa columna.
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            toDoList.removeAtIndex(indexPath.row)
+            //Elimino de la "base de datos" el valor de la columna especificada.
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            //Recargo la tabla.
+            toDoTable.reloadData()
+        }
+    }
+    
+    //Este metodo recarga la informacion en la tabla.
+    override func viewDidAppear(animated: Bool) {
+        toDoTable.reloadData()
     }
 
 
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func addItemBtn(sender: AnyObject) {
+        //Añadimos al array lo que el usuario introduzca en el textfield.
+        toDoList.append(textField.text)
+        textField.text = ""
+        
+        //Con esta linea de codigo almacenamos para siempre lo que añadimos en la tabla.
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +29,17 @@ class SecondViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Metodo para controlar el teclado
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+    
+    //Idem
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
